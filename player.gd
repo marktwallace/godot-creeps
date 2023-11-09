a4640c59facb3fa73ba8f45f91c0e1bdf90a1ce0
Mark Wallace <mark@leker.to>
2023-11-08 19:28:41 -0800

    Testing Main

--- player.gd
+++ player.gd
@@ -6,14 +6,17 @@ signal hit
 var screen_size # Size of the game window.
 
 func _ready():
+	print("ready Player 1")
 	screen_size = get_viewport_rect().size
 	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
+		print("right")
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
+		print("left")
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
