0964e048b8cc1d98d1e23cfaeff495ee31850fac
Mark Wallace <mark@leker.to>
2023-11-09 23:45:06 -0800

    Works, no controller

--- main.gd
+++ main.gd
@@ -4,22 +4,30 @@ extends Node
 var score
 
 func _ready():
-	new_game()
+	pass
 
 func _process(delta):
 	pass
 
 func game_over():
+	$Music.stop()
+	$DeathSound.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	$Music.play()
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
--- player.gd
+++ player.gd
@@ -13,10 +13,8 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
-		print("right")
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
-		print("left")
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
